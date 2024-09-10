64b2ed525b527a40b44019a2dcc13a78a0e8c4c7
rairongf <rairon.gf@gmail.com>
2024-09-10 14:56:51 -0300

    chore: add config module

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './interceptors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    //cors: true,
+    //bufferLogs: true,
+  });
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
---
+++ api/src/modules/config/module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule,
+} from '@nestjs/config';
+import Joi from 'joi';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+        //S3_STORAGE_URL: Joi.string().required(),
+        //S3_ACCESS_KEY_ID: Joi.string().required(),
+        //S3_SECRET_ACCESS_KEY: Joi.string().required(),
+      }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule { }

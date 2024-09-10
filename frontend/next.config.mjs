0df7ccfc5abba318cb862dbe55e5896d1ed3022e
rairongf <rairon.gf@gmail.com>
2024-09-10 19:31:51 -0300

    chore: change next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/channels/@me',
+        permanent: true,
+      },
+      {
+        source: '/channels(/?$)',
+        destination: '/channels/@me',
+        permanent: true,
+      }
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      }
+    ]
+  },
+};
 
 export default nextConfig;

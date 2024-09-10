0bf63117559a71ae9a6f7719a4d0a45f9664af92
rairongf <rairon.gf@gmail.com>
2024-09-10 19:31:37 -0300

    chore: change tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,19 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'selector',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;

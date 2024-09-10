fefec0291fa4216a915d577f39d06d4fad66e5b5
rairongf <rairon.gf@gmail.com>
2024-09-10 19:35:02 -0300

    refactor: change default layout component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,15 @@
-import type { Metadata } from "next";
-import { Nunito_Sans } from 'next/font/google';
-import "./globals.css";
+import { ThemeProvider } from "@/modules/theme";
+import { Metadata, Viewport } from "next";
+import { Nunito_Sans } from "next/font/google";
+import "./styles/globals.css";
+import "./styles/material_icons.css";
 
-const nunitoSans = Nunito_Sans({ subsets: ['latin'] });
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
 
 export const metadata: Metadata = {
   title: "Chat App",
@@ -17,9 +24,14 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className={`${nunitoSans.className} w-screen h-screen`}
+        className={`${nunitoSans.className} w-screen h-screen overflow-hidden`}
       >
-        {children}
+        <ThemeProvider>
+          <div className="flex md:flex-row w-full h-full">
+            {/* <LeftNavigationBar /> */}
+            {children}
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );

1a1282acb128266d963fb167d5919355f717d8dc
rairongf <rairon.gf@gmail.com>
2024-09-10 16:38:01 -0300

    fix: build issues

--- api/src/modules/config/module.ts
+++ api/src/modules/config/module.ts
@@ -3,15 +3,16 @@ import {
   ConfigService,
   ConfigModule as NestConfigModule,
 } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: ['.env.development.local'],
       validationSchema: Joi.object({
         MONGO_URI: Joi.string().required(),
         PORT: Joi.number().required(),
+        NODE_ENV: Joi.string().required(),
         //S3_STORAGE_URL: Joi.string().required(),
         //S3_ACCESS_KEY_ID: Joi.string().required(),
         //S3_SECRET_ACCESS_KEY: Joi.string().required(),

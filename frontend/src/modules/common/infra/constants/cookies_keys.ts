43ad26c324bc863c178304fbdbef50d697cc7cc2
rairongf <rairon.gf@gmail.com>
2024-09-10 21:41:06 -0300

    chore: add cookies keys constants

---
+++ frontend/src/modules/common/infra/constants/cookies_keys.ts
@@ -0,0 +1,5 @@
+export const CookiesKeys = {
+  refreshToken: 'chat_app.refresh_token',
+  accessToken: 'chat_app.access_token',
+  keepAuth: 'chat_app.keep_auth',
+};
---
+++ frontend/src/modules/common/infra/constants/index.ts
@@ -0,0 +1,1 @@
+export * from './cookies_keys';

5c9859dd8eddfe7f2605871150cb8148a6041b7f
rairongf <rairon.gf@gmail.com>
2024-09-17 21:16:41 -0300

    chore: update light theme entry

--- frontend/src/modules/theme/constants/light_theme_colors.ts
+++ frontend/src/modules/theme/constants/light_theme_colors.ts
@@ -5,7 +5,7 @@ export const LightThemeColors: ThemeColors = {
   backgroundSecondary: 'bg-[#F2F3F5]',
   backgroundTertiary: 'bg-[#E3E5E8]',
   focusBackground: 'bg-[#E0E1E5]',
-  hoverMessageBackground: 'hover:bg-[#F2F3F5]',
+  hoverMessageBackground: 'hover:bg-[#E3E5E8]',
   foreground: 'bg-[#171717]',
   divider: 'bg-[#171717]',
 }

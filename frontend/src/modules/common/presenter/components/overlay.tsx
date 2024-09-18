da55ab3337e0b91f6f53c23ece0017a00bf9bae4
rairongf <rairon.gf@gmail.com>
2024-09-17 21:15:22 -0300

    chore: add overlay component

---
+++ frontend/src/modules/common/presenter/components/overlay.tsx
@@ -0,0 +1,16 @@
+import { twJoin } from "tailwind-merge";
+
+export type OverlayProps = React.PropsWithChildren;
+
+export function Overlay({ children }: OverlayProps) {
+  return (
+    <div
+      className={twJoin(
+        "absolute top-0 left-0 right-0 bottom-0 backdrop-blur-sm",
+        "flex justify-center items-center"
+      )}
+    >
+      {children}
+    </div>
+  );
+}

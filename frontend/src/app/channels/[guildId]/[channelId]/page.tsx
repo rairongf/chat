4383578a8a790fd5c1ee201de20642bbf86d89af
rairongf <rairon.gf@gmail.com>
2024-09-23 16:16:41 -0300

    refactor: initialize active chat from route param

--- frontend/src/app/channels/[guildId]/[channelId]/page.tsx
+++ frontend/src/app/channels/[guildId]/[channelId]/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 
 import { ActiveChatView } from "@/modules/common";
-import { UserGuildRoutePathMask } from "@/modules/direct_messages";
 import { usePathname } from "next/navigation";
 
 export default function ServerChannelPage() {
   const pathname = usePathname();
   const pathSegments = pathname.substring(1).split("/");
-  const guildIdOrAlias = pathSegments[1];
-
-  const showDirectMessagesPage = guildIdOrAlias == UserGuildRoutePathMask;
 
   return (
     <>
-      {showDirectMessagesPage && (
-        <ActiveChatView
-          guildIdOrMask={guildIdOrAlias}
-          channelId={pathSegments[2]}
-        />
-      )}
+      <ActiveChatView
+        guildIdOrMask={pathSegments[1]}
+        channelId={pathSegments[2]}
+      />
     </>
   );
 }
--- frontend/src/modules/direct_messages/presenter/page_content.tsx
+++ frontend/src/modules/direct_messages/presenter/page_content.tsx
@@ -1,4 +1,5 @@
 import { Row } from "@/modules/common";
+import { usePathname } from "next/navigation";
 import { RecentChatsTab } from "./components";
 
 export function DirectMessagesPageContent({
@@ -6,10 +7,17 @@ export function DirectMessagesPageContent({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const pathSegments = pathname.substring(1).split("/");
+
+  const containsChannelId = pathSegments.length > 2;
+
   return (
     <Row className={`justify-start items-stretch w-full`}>
       {/* Recent chats tabs */}
       <RecentChatsTab />
+
+      {!containsChannelId && <></>}
 
       {children}
     </Row>

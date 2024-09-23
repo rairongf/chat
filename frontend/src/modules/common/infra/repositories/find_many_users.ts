f7413e115ac76d30b27ff803f5499db0335537cf
rairongf <rairon.gf@gmail.com>
2024-09-23 13:38:03 -0300

    refactor: set paginated return type

--- frontend/src/modules/common/infra/repositories/find_many_users.ts
+++ frontend/src/modules/common/infra/repositories/find_many_users.ts
@@ -1,8 +1,8 @@
 import { BaseRepository } from '@/modules/common/infra/models';
-import { User } from '../../domain';
+import { PaginatedData, User } from '../../domain';
 import { api } from '../services';
 
-export type IFindManyUsersRepository = BaseRepository<User[]>;
+export type IFindManyUsersRepository = BaseRepository<PaginatedData<User>>;
 
 export const findManyUsers: IFindManyUsersRepository = () => {
   return api.get(`/users`);

90821cf44dcaae33234e9a5c963c63de080ed741
rairongf <rairon.gf@gmail.com>
2024-10-01 14:10:35 -0300

    chore: change language resources

--- frontend/src/modules/language/presenter/en_US_resource.ts
+++ frontend/src/modules/language/presenter/en_US_resource.ts
@@ -4,8 +4,8 @@ export const enUS_Resource: LocaleResource = {
   login: {
     title: 'Welcome back!',
     subtitle: "We're excited to see you again!",
-    emailInputLabel: 'Email *',
-    passwordInputLabel: 'Password *',
+    emailInputLabel: 'Email',
+    passwordInputLabel: 'Password',
     forgotPasswordButtonLabel: 'Forgot password?',
     logInButtonLabel: 'Log In',
     dontHaveAnAccount: 'Need an account?',
--- frontend/src/modules/language/presenter/pt_BR_resource.ts
+++ frontend/src/modules/language/presenter/pt_BR_resource.ts
@@ -4,8 +4,8 @@ export const ptBR_Resource: LocaleResource = {
   login: {
     title: 'Boas-vindas de volta!',
     subtitle: 'Estamos muito animados em te ver novamente!',
-    emailInputLabel: 'E-mail *',
-    passwordInputLabel: 'Senha *',
+    emailInputLabel: 'E-mail',
+    passwordInputLabel: 'Senha',
     forgotPasswordButtonLabel: 'Esqueceu a senha?',
     logInButtonLabel: 'Entrar',
     dontHaveAnAccount: 'Precisando de uma conta?',

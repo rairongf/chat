4d8771245ccba3707fea8b03b348b87b5c6fc34f
rairongf <rairon.gf@gmail.com>
2024-10-08 11:47:55 -0300

    fix: attribute names

--- frontend/src/modules/common/presenter/components/inputs/checkbox.tsx
+++ frontend/src/modules/common/presenter/components/inputs/checkbox.tsx
@@ -38,9 +38,9 @@ export function CheckboxInput({
         viewBox="0 0 24 24"
         fill="none"
         stroke="white"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <polyline points="20 6 9 17 4 12"></polyline>
       </svg>

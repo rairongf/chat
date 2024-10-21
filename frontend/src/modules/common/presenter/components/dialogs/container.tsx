0a8fdf3a06924568d31bd300d652ab532faf80ca
rairongf <rairon.gf@gmail.com>
2024-10-21 16:15:30 -0300

    chore: change dialog container styles

--- frontend/src/modules/common/presenter/components/dialogs/container.tsx
+++ frontend/src/modules/common/presenter/components/dialogs/container.tsx
@@ -1,25 +1,34 @@
 import { Button, Column, Icon } from "@/modules/common";
 import { useTheme } from "@/modules/theme";
-import { twJoin } from "tailwind-merge";
+import { twJoin, twMerge } from "tailwind-merge";
 
 export type DialogContainerProps = React.PropsWithChildren<{
   onClose: () => void;
-}>;
+}> &
+  Pick<React.HTMLAttributes<HTMLDivElement>, "className">;
 
-export function DialogContainer({ children, onClose }: DialogContainerProps) {
+export function DialogContainer({
+  children,
+  className,
+  onClose,
+}: DialogContainerProps) {
   const { theme } = useTheme();
 
   return (
     <Column
-      className={twJoin(
+      className={twMerge(
         "relative w-[27.5rem] min-h-52 max-h-[85%] rounded",
-        theme.colors.background.primary
+        theme.colors.background.primary,
+        className
       )}
     >
-      <Button className="absolute right-3 top-3" onClick={() => onClose()}>
+      <Button
+        className="absolute right-3 top-3 size-6"
+        onClick={() => onClose()}
+      >
         <Icon
           name={"close"}
-          className={twJoin("text-2xl", theme.colors.text.base)}
+          className={twJoin("text-2xl leading-none", theme.colors.text.base)}
         />
       </Button>
       {children}
--- frontend/src/modules/common/presenter/components/dialogs/index.ts
+++ frontend/src/modules/common/presenter/components/dialogs/index.ts
@@ -1,6 +1,7 @@
 import { DialogContainer } from './container';
 
 export * from './add_guild_dialog';
+export type { DialogContainerProps } from './container';
 export * from './context';
 
 export const Dialog = {
